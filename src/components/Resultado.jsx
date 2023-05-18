ccebf5069c9475beca08c4514e821b50775c6475
Ivan Enrique Vasquez Ramirez <ivanvasquez@Ivans-MacBook-Air.local>
2023-05-18 14:07:54 -0600

    Finalizando el componente de Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,11 +1,36 @@
 import useCotizador from "../hooks/useCotizador"
+import { planes } from "../constants";
+
 const Resultado = () => {
-  const { resultado} = useCotizador()
+  const { resultado, datos} = useCotizador()
+  const {marca,year,plan} = datos;
+
+  // Pasar a number el plan del state porque como viene el form, nos lo da en string
+  // Deberia haberlo hecho igual con marca pero en el form en value le puse marca.nombre y no .id
+  const [nombrePlan] = planes.filter( p => p.id ===  Number(plan))
 
   if(resultado===0) return null
   
   return (
-    <div>{resultado}</div>
+    <div className="bg-gray-100 text-center shadow-md mt-5 p-5">
+      <h2 className=" text-gray-600 font-bold text-2xl">RESUMEN</h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {marca}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año del auto: </span>
+        {year}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotizacion: </span>
+        {resultado}
+      </p>
+    </div>
   )
 }
 
